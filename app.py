445e7d9a4d72d5aeb623fecb5b65ca656b235cce
tiffanyng23 <ngtiffany23@gmail.com>
2026-02-02 17:41:13 -0500

    Revised descriptions and removed violin plot, and reorganized files.

--- app.py
+++ app.py
@@ -6,7 +6,25 @@ import dash_bootstrap_components as dbc
 import os
 
 #DATASET
-dataset= pd.read_csv("gdm_vat_data_cleaned.csv")
+# cleaned data
+dataset= pd.read_csv("datasets/gdm_vat_data_cleaned.csv")
+
+#convert values to a more user-friendly format
+# type of delivery
+dataset["type_of_delivery"] = dataset["type_of_delivery"].map({
+    0: "Vaginal",
+    1: "C-section"
+})
+#GDM status
+dataset["gestational_dm"] = dataset["gestational_dm"].map({
+    0: "Non-GDM",
+    1: "GDM"
+})
+#ethnicity
+dataset["ethnicity"] = dataset["ethnicity"].map({
+    0:"Non-White",
+    1: "White"
+})
 
 #categorical variables
 cat_var_data = dataset[["ethnicity", "pregnancies", "type_of_delivery", "gestational_dm"]]
@@ -22,30 +40,30 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 server = app.server
 
 #creating cards
+#Summary of Study Card
 card_description = dbc.Card([
     html.Div(
         [
-            html.P("BMI has been long associated with gestational diabetes. However, BMI does not differentiate between muscle, fat, and bone mass; and does not indicate fat distribution around the body. Individuals with a tendency to store visceral fat (e.g central/lower belly fat) are at a higher risk of metabolic diseases, therefore visceral fat may be a viable alternative to use to predict cardiometabolic disease risk. In the study by Rocha et al. (2020), visceral adipose tissue was measured in a cohort of 133 women in the first half of pregnancy to assess its ability to predict gestational diabetes at the end of pregnancy.",
-            className="card-text"),
-            html.P("Below are a series of visualizations created using the data from the study which can be used to identify differences in clinical markers between those with and without gestational diabetes (GDM). Note that for GDM status, non-GDM=0 and GDM=1. For the variable ethnicity, white=1 and non-white=0. For the variable type of delivery, vaginal=0 and cesarean section=1."),
-            html.P("Reference: Rocha ADS, Bernardi JR, Matos S, et al. Maternal visceral adipose tissue during the first half of pregnancy predicts gestational diabetes at the time of delivery - a cohort study. PLoS One. 2020;15(4):e0232155. Published 2020 Apr 30. doi:10.1371/journal.pone.0232155",
-            className="card-text"),
+            html.P("Below are a series of interactive, user-driven visualizations that can be used to analyze a dataset from a study assessing differences in clinical markers between those with and without gestational diabetes (GDM)."),
+            html.A("Link to Study", href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7192370/", target="_blank"),
+            html.P("Reference: Rocha ADS, Bernardi JR, Matos S, et al. Maternal visceral adipose tissue during the first half of pregnancy predicts gestational diabetes at the time of delivery - a cohort study. PLoS One. 2020;15(4):e0232155. Published 2020 Apr 30. doi:10.1371/journal.pone.0232155"),
         ]),
     ], 
     body=True, color="lightgrey"
 )
 
+#Visualizations Cards
 card_cat_var = dbc.Card([
         html.Div([
-            html.Label(children='Choose a Graph:'),
+            html.Label(children='Select Graph:'),
             dcc.Dropdown(
-                options = ["violin", "histogram"], 
-                value="violin", id="violin_hist"
+                options = ["histogram"], 
+                value="histogram", id="hist"
                 ),
             html.Label(children='Choose a Categorical Variable:'),
             dcc.Dropdown(
                 options=["ethnicity", "pregnancies", "type_of_delivery"], 
-                value="pregnancies", id="violin_hist_var"),
+                value="pregnancies", id="hist_var"),
             ]),
         ], 
         body=True, color="lightgrey",
@@ -56,13 +74,13 @@ card_cont_var = dbc.Card([
             html.Label('Choose a Graph:'),
             dcc.Dropdown(
                 options=["histogram", "boxplot"], 
-                value="boxplot", id="hist_box"),
+                value="boxplot", id="box"),
             html.Label('Choose a Continuous Variable:'),
             dcc.Dropdown(
                 options=["mean_diastolic_bp", "mean_systolic_bp", "central_armellini_fat",
                         "first_fasting_glucose","bmi_pregestational", "child_birth_weight", 
                         "gestational_age_at_birth", "current_gestational_age", "age","gestational_dm"], 
-                value="first_fasting_glucose", id='hist_box_var'),
+                value="first_fasting_glucose", id='box_var'),
         ]),
     ],
     body=True, color="lightgrey",
@@ -100,8 +118,6 @@ card_heatmap = dbc.Card([
 ], body=True, color="lightgrey")
 
 
-
-
 #App layout
 app.layout = dbc.Container([
     dbc.Row([
@@ -113,20 +129,20 @@ app.layout = dbc.Container([
         html.Hr()
     ]),
     dbc.Tabs([
-        dbc.Tab(label = "Visualizations of Variables", children = [
+        dbc.Tab(label = "Univariate Visualizations", children = [
             #Violin plot
             dbc.Row([
                 dbc.Col(card_cat_var, width=3),
-                dbc.Col(dcc.Graph(figure={}, id= "controls-and-violin-graph"), width=9),
+                dbc.Col(dcc.Graph(figure={}, id= "controls-and-hist-graph"), width=9),
             ],align= "center",
             ),
             dbc.Row([
                 dbc.Col(card_cont_var, width=3),
-                dbc.Col(dcc.Graph(figure={}, id= "controls-and-hist-graph"), width=9)
+                dbc.Col(dcc.Graph(figure={}, id= "controls-and-box-graph"), width=9)
             ], align="center"),
         ]),
 
-        dbc.Tab(label= "Relationship Between Variables", children = [
+        dbc.Tab(label= "Multivariate Visualizations", children = [
             dbc.Row([
                 dbc.Col(card_scatter, width=3),
                 dbc.Col(dcc.Graph(figure={}, id= "controls-and-scatter-graph"), width=9)
@@ -140,28 +156,16 @@ app.layout = dbc.Container([
 ])
 
 
-
 #CALLBACKS
 #adding callback for violin plot
 @callback(
-    Output(component_id="controls-and-violin-graph", component_property="figure"),
-    Input(component_id='violin_hist', component_property="value"),
-    Input(component_id='violin_hist_var', component_property="value")
+    Output(component_id="controls-and-hist-graph", component_property="figure"),
+    Input(component_id='hist', component_property="value"),
+    Input(component_id='hist_var', component_property="value")
 )
 
 def update_violin_box(violin_hist, violin_hist_var):
-    if violin_hist == "violin":
-        fig = px.violin(cat_var_data, 
-                        x=violin_hist_var, 
-                        color="gestational_dm", 
-                        facet_col ="gestational_dm",
-                        title=f"Violin Plot of {violin_hist_var}",
-                        labels={"gestational_dm":"GDM Status"},
-                        template="seaborn",
-                        )
-        fig.update_layout(xaxis_title=f"{violin_hist_var}")
-    else:
-        fig = px.histogram(cat_var_data, 
+    fig = px.histogram(cat_var_data, 
                         x=violin_hist_var, 
                         color="gestational_dm", 
                         facet_col="gestational_dm",
@@ -169,41 +173,26 @@ def update_violin_box(violin_hist, violin_hist_var):
                         title=f"Histogram of {violin_hist_var}",
                         labels={"gestational_dm":"GDM Status"},
                         template="seaborn",
-                        )
-        fig.update_layout(xaxis_title=f"{violin_hist_var}", yaxis_title="Probability Density")
+                    )
+    fig.update_layout(xaxis_title=f"{violin_hist_var}", yaxis_title="Probability Density")
     return fig
 
 #adding callback for histogram and boxplot
 @callback(
-    Output(component_id="controls-and-hist-graph", component_property="figure"),
-    Input(component_id='hist_box', component_property="value"),
-    Input(component_id='hist_box_var', component_property="value")
-    
+    Output(component_id="controls-and-box-graph", component_property="figure"),
+    Input(component_id='box', component_property="value"),
+    Input(component_id='box_var', component_property="value")
 )
-
 def update_hist_box(hist_box, hist_box_var):
-    if hist_box == "histogram":
-        fig = px.histogram(cont_var_data, 
-                        x=hist_box_var, 
-                        color="gestational_dm", 
-                        facet_col="gestational_dm",
-                        histnorm="probability density",
-                        title=f"Histogram of {hist_box_var}",
-                        labels={"gestational_dm":"GDM Status"},
-                        template="seaborn",
-                        )
-        fig.update_layout(xaxis_title=f"{hist_box_var}", yaxis_title="Probability Density")
-    else:
-        fig = px.box(cont_var_data,
-                    x=hist_box_var, 
-                    color="gestational_dm", 
-                    facet_col="gestational_dm",
-                    title=f"Boxplot of {hist_box_var}",
-                    labels={"gestational_dm":"GDM Status"},
-                    template="seaborn",)
-        fig.update_layout(xaxis_title=f"{hist_box_var}")
+    fig = px.box(cont_var_data,
+                x=hist_box_var, 
+                color="gestational_dm", 
+                facet_col="gestational_dm",
+                title=f"Boxplot of {hist_box_var}",
+                labels={"gestational_dm":"GDM Status"},
+                template="seaborn",)
+    fig.update_layout(xaxis_title=f"{hist_box_var}")
     return fig
-
 
 
 #adding callback for scatterplot
@@ -212,7 +201,6 @@ def update_hist_box(hist_box, hist_box_var):
     Input(component_id='x1', component_property="value"),
     Input(component_id='y1', component_property="value")
 )
-
 def update_scatter(x1, y1):
     fig = px.scatter(cont_var_data, 
                     x=x1,
@@ -249,8 +237,7 @@ def update_heatmap(variables):
 
     return fig
 
-
 #Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
     
